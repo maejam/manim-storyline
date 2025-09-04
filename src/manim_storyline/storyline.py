eef627583486645880324c01a7ecf63e0189e6a0
maejam <git.rejoice553@passmail.net>
2025-09-04 16:05:38 +0200

    feat: Add 2 distinct dots (in_dots and out_dots) for Story refactor: Move dots definition to Story. Keep dot display control in Transition

--- src/manim_storyline/storyline.py
+++ src/manim_storyline/storyline.py
@@ -1,63 +1,57 @@
-import typing as t
+from dataclasses import dataclass
+from dataclasses import field
 
 import manim as m  # type: ignore[import-untyped]
-from typing_extensions import Unpack
+import manim.typing as mt  # type: ignore[import-untyped]
 
 from .transition import Slide
 from .transition import Transition
 
 
+class StoryLineException(Exception):
+    pass
+
+
 class StoryLineScene(m.MovingCameraScene):  # type:ignore[misc]
     def setup(self) -> None:
-        self.world = m.VGroup()
+        self.world = m.Group()
+        self.stories: dict[str, "Story"] = {}
         self.origin = self.head = self.create_story("origin")
+        self.origin.in_dot = self.origin.out_dot
 
-    def create_story(
-        self,
-        name: str,
-        frame: m.VMobject | None = None,
-        frame_display: type[m.Animation] | bool = False,
-        margin: float = 0,
-    ) -> "Story":
-        if not frame:
-            frame = m.Rectangle(width=16, height=9)
-        story = Story(
-            self, name=name, frame=frame, frame_display=frame_display, margin=margin
-        )
+    def create_story(self, name: str, *args, **kwargs) -> "Story":  # type: ignore[no-untyped-def]
+        # TODO: find a way to type method without repeating dataclass signature (using fields?)
+        story = Story(self, name, *args, **kwargs)
         return story
 
     def transition_to(self, story: "Story", transition: Transition = Slide()) -> None:
         transition._transition(story, self)
         self.head = story
 
-    def add_to_world(self, *mobjects: m.VMobject) -> None:
-        self.world.add(mobjects)
+    def add_to_world(self, *mobjects: m.Mobject) -> None:
+        self.world.add(*mobjects)
 
     def show_world(self, margin: float = 2) -> None:
-        self.play(self.camera.auto_zoom(self.world, margin=margin))  # pyright: ignore[reportAttributeAccessIssue]
+        try:
+            self.play(self.camera.auto_zoom(self.world, margin=margin))  # pyright: ignore[reportAttributeAccessIssue]
+        except TypeError as e:
+            raise StoryLineException("The world is probably empty") from e
 
 
-class StoryParams(t.TypedDict):
-    name: str
-    frame: m.VMobject
-    frame_display: type[m.Animation] | bool
-    margin: float
-
-
+@dataclass
 class Story:
-    def __init__(
-        self,
-        scene: "StoryLineScene",
-        **kwargs: Unpack[StoryParams],
-    ) -> None:
-        """
-        DO NOT USE DIRECTLY, use the `StoryLine.create_story` factory method instead
-        """
-        self.scene = scene
-        self.name = kwargs["name"]
-        self.frame = kwargs["frame"]
-        self.frame_display = kwargs["frame_display"]
-        self.margin = kwargs["margin"]
+    scene: StoryLineScene
+    name: str
+    frame: m.VMobject = field(default_factory=lambda: m.Rectangle(width=16, height=9))
+    in_dot: m.Dot = field(default_factory=lambda: m.Dot())
+    in_dot_direction: mt.Vector3D = field(default_factory=lambda: m.DOWN)
+    in_dot_buffer: float = 5
+    out_dot: m.Dot = field(default_factory=lambda: m.Dot())
+    out_dot_direction: mt.Vector3D = field(default_factory=lambda: m.DOWN)
+    out_dot_buffer: float = 5
+
+    def __post_init__(self) -> None:
+        self.scene.stories[self.name] = self
 
     def add(self, mobject: m.Mobject) -> None:
         position = self.frame.get_center()
--- src/manim_storyline/transition.py
+++ src/manim_storyline/transition.py
@@ -6,15 +6,21 @@ from dataclasses import field
 
 import manim as m  # type: ignore[import-untyped]
 import manim.typing as mt  # type: ignore[import-untyped]
+import numpy as np
 
 if t.TYPE_CHECKING:
     from .storyline import Story
     from .storyline import StoryLineScene
 
 
+class TransitionError(Exception):
+    pass
+
+
 @dataclass
 class Transition(ABC):
-    zoom_out_margin: float = 2
+    target_frame_display: bool | type[m.Animation] = True
+    zoom_out_margin: float = 5
     zoom_in_margin: float = 2
 
     @abstractmethod
@@ -28,13 +34,13 @@ class Transition(ABC):
         self.scene.add_to_world(story.frame)
 
     def zoom_in_on_story(self, story: "Story") -> m.Animation:
-        animation = self.scene.camera.auto_zoom(story.frame, margin=story.margin)  # pyright: ignore[reportAttributeAccessIssue]
+        animation = self.scene.camera.auto_zoom(story.frame, margin=self.zoom_in_margin)  # pyright: ignore[reportAttributeAccessIssue]
         return animation
 
     def zoom_out_from_story(self, story: "Story") -> m.Animation:
         animation = self.scene.camera.auto_zoom(  # pyright: ignore[reportAttributeAccessIssue]
             story.frame,
-            margin=story.margin + self.zoom_out_margin,
+            margin=self.zoom_out_margin,
         )
         return animation
 
@@ -43,11 +49,11 @@ class Transition(ABC):
         return animation
 
     def animate_frame_creation(self, story: "Story") -> m.Animation:
-        if type(story.frame_display) is bool:
-            raise TypeError(
-                "`Transition.animate_frame_creation` should only be called when Story.frame_display is a Manim Animation. Use `Transition.add_frame_to_scene` instead"
+        if type(self.target_frame_display) is bool:
+            raise TransitionError(
+                "`Transition.animate_frame_creation` should only be called when Transition.target_frame_display is a Manim Animation. Use `Transition.add_frame_to_scene` instead"
             )
-        animation = story.frame_display(story.frame)
+        animation = self.target_frame_display(story.frame)
         self.scene.add_to_world(story.frame)
         return animation
 
@@ -58,14 +64,14 @@ class Slide(Transition):
         super()._transition(target, scene)
 
         # Target frame add
-        if target.frame_display is True:
+        if self.target_frame_display is True:
             self.add_frame_to_scene(target)
 
         self.scene.play(self.zoom_out_from_story(self.head))
         self.scene.play(self.slide_to_story(target))
 
         # Target frame create
-        if target.frame_display and target.frame_display is not True:
+        if self.target_frame_display and self.target_frame_display is not True:
             self.scene.play(self.animate_frame_creation(target))
 
         self.scene.play(self.zoom_in_on_story(target))
@@ -73,7 +79,7 @@ class Slide(Transition):
     def slide_to_story(self, story: "Story") -> m.Animation:
         animation = self.scene.camera.auto_zoom(  # pyright: ignore[reportAttributeAccessIssue]
             story.frame,
-            margin=story.margin + self.zoom_in_margin,
+            margin=self.zoom_in_margin,
         )
         return animation
 
@@ -85,34 +91,25 @@ class FreeStoryLine(Transition):
     display_arrow: bool = True
     display_head2dot_line: bool = True
     display_dot2target_line: bool = True
-    head_dot: m.Dot = field(default_factory=lambda: m.Dot())
-    target_dot: m.Dot = field(default_factory=lambda: m.Dot())
     arrow: m.Arrow = field(default_factory=lambda: m.Arrow())
     head2dot_line: m.Line = field(default_factory=lambda: m.Line())
     dot2target_line: m.Line = field(default_factory=lambda: m.Line())
     dot2target_line_through: tuple[mt.Point3DLike, ...] = ()
-    head_dot_direction: mt.Vector3D = field(default_factory=lambda: m.DOWN)
-    head_dot_buffer: float = 5
-    target_dot_direction: mt.Vector3D = field(default_factory=lambda: m.DOWN)
-    target_dot_buffer: float = 5
-
-    _dot_x_value_constraint: bool = False
-    _dot_y_value_constraint: bool = False
 
     def _transition(self, target: "Story", scene: "StoryLineScene") -> None:
         super()._transition(target, scene)
 
         # Target frame add
-        if target.frame_display is True:
+        if self.target_frame_display is True:
             self.add_frame_to_scene(target)
 
         self.scene.play(self.zoom_out_from_story(self.head))
-        dot1 = self.add_dot_next_to_story(self.head)
 
         # Head to dot line
+        dot1 = self.adjust_dot_position(self.head, "out")
         if self.display_head2dot_line:
             self.scene.play(
-                m.Create(self.line_from_head_to_dot(self.head)),
+                m.Create(self.line_from_head_to_dot()),
                 self.move_camera_frame_to(dot1),
             )
         else:
@@ -125,7 +122,7 @@ class FreeStoryLine(Transition):
             self.scene.play(m.Create(dot1))
             self.scene.add_to_world(dot1)
 
-        dot2 = self.add_dot_next_to_story(target)
+        dot2 = self.adjust_dot_position(self.target, "in")
 
         # Arrow
         if self.display_arrow:
@@ -154,26 +151,20 @@ class FreeStoryLine(Transition):
             )
 
         # Target frame create
-        if target.frame_display and target.frame_display is not True:
+        if self.target_frame_display and self.target_frame_display is not True:
             self.scene.play(self.animate_frame_creation(target))
         self.scene.play(self.zoom_in_on_story(target))
 
-    def add_dot_next_to_story(self, story: "Story") -> m.Dot:
-        if story is self.head:
-            dot = self.head_dot
-            direction = self.head_dot_direction
-            buffer = self.head_dot_buffer
+    def adjust_dot_position(self, story: "Story", in_or_out: str) -> m.Dot:
+        if in_or_out == "in":
+            dot = story.in_dot
+            direction = story.in_dot_direction
+            buffer = story.in_dot_buffer
         else:
-            dot = self.target_dot
-            direction = self.target_dot_direction
-            buffer = self.target_dot_buffer
-
-        dot.next_to(story.frame, direction, buff=buffer)
-
-        if self._dot_x_value_constraint and story is self.target:
-            dot.set_x(self.head_dot.get_x())
-        if self._dot_y_value_constraint and story is self.target:
-            dot.set_y(self.head_dot.get_y())
+            dot = story.out_dot
+            direction = story.out_dot_direction
+            buffer = story.out_dot_buffer
+        dot.next_to(story.frame, direction, buffer)
         return dot
 
     def animate_arrow(self, start: m.Mobject, end: m.Mobject) -> m.Animation:
@@ -182,20 +173,21 @@ class FreeStoryLine(Transition):
         self.scene.add_to_world(self.arrow)
         return animation
 
-    def line_from_head_to_dot(self, story: "Story") -> m.Line:
-        direction = self.head_dot_direction
+    def line_from_head_to_dot(self) -> m.Line:
+        direction = self.head.out_dot_direction
         self.head2dot_line.put_start_and_end_on(
-            story.frame.get_critical_point(direction), self.head_dot.get_center()
+            self.head.frame.get_critical_point(direction),
+            self.head.out_dot.get_center(),
         )
         self.scene.add_to_world(self.head2dot_line)
         return self.head2dot_line
 
     def line_from_dot_to_target(self, story: "Story") -> list[m.Line]:
-        direction = self.target_dot_direction
+        direction = self.target.in_dot_direction
         through = self.dot2target_line_through + (
             story.frame.get_critical_point(direction),
         )
-        start = self.target_dot.get_center()
+        start = self.target.in_dot.get_center()
         lines: list[m.Line] = []
         for point in through:
             lines.append(self.dot2target_line.copy().put_start_and_end_on(start, point))
@@ -206,26 +198,40 @@ class FreeStoryLine(Transition):
 
 @dataclass
 class HorizontalStoryLine(FreeStoryLine):
-    _dot_y_value_constraint: bool = True
+    def adjust_dot_position(self, story: "Story", in_or_out: str) -> m.Dot:
+        dot = super().adjust_dot_position(story, in_or_out)
+        if story is self.target:
+            dot.set_y(self.head.out_dot.get_y())
+        else:
+            dot.set_y(self.head.in_dot.get_y())
+        return dot
 
 
 @dataclass
 class VerticalStoryLine(FreeStoryLine):
-    _dot_x_value_constraint: bool = True
+    def adjust_dot_position(self, story: "Story", in_or_out: str) -> m.Dot:
+        dot = super().adjust_dot_position(story, in_or_out)
+        if story is self.target:
+            dot.set_x(self.head.out_dot.get_x())
+        else:
+            dot.set_x(self.head.in_dot.get_x())
+        return dot
 
 
 @dataclass
-class BezierStoryLine(FreeStoryLine):
-    def animate_arrow(self, start: m.Mobject, end: m.Mobject) -> m.Animation:
-        animation = m.Create(
-            m.CubicBezier(
-                start.get_center(),
-                start.get_center() + m.RIGHT + m.DOWN,
-                end.get_center(),
-                end.get_center() + m.LEFT + m.UP,
-            )
-        )
-        # self.arrow.put_start_and_end_on(start.get_center(), end.get_center())
-        # animation = m.GrowArrow(self.arrow)
-        # self.scene.add_to_world(self.arrow)
-        return animation
+class PolyFitStoryLine(FreeStoryLine):
+    def get_stories_dot_coords(self) -> tuple[np.ndarray, np.ndarray]:
+        x = np.array([])
+        y = np.array([])
+        for num, story in enumerate(self.scene.stories.values()):
+            x[num] = story.dot.get_x()
+            y[num] = story.dot.get_x()
+        return x, y
+
+    # def animate_arrow(self, start: m.Mobject, end: m.Mobject) -> m.Animation:
+    #     poly = npp.Polynomial.fit(self.get_stories_dot_coords())
+    #     print(poly)
+    # self.arrow.put_start_and_end_on(start.get_center(), end.get_center())
+    # animation = m.GrowArrow(self.arrow)
+    # self.scene.add_to_world(self.arrow)
+    # return animation
